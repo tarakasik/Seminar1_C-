17b0e00d55f66edb041741eb73c3228de0629777
Stanislav Volokhov <lamantina123@gmail.com>
2022-08-06 23:56:32 +0400

    add

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// // aka.ms/new-console-template for more information
+// // Console.WriteLine("Hello, World!");
+// Console.Clear();
+// // int i = 0;
+
+
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int sqrt = number * number;
+// // number *= number;
+// // sqrt = Convert.ToInt32(Math.Pow(number, 2));
+
+// Console.Write("Число в квадрате: " + sqrt);
+
+// Console.Write("Input first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 == num2*num2);
+// {
+//     Console.Write("First number");
+// }
+
+
+
+
+// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+	// 3 -> Среда 
+// 5 -> Пятница
+
+
+// Console.Write("Input number of the day: ");
+
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day < 1 || day > 7)
+// {
+//     Console.Write("Incorrect number");
+// }
+// else
+// {
+//     if (day == 1)
+//     {
+//         Console.Write("Monday");
+//     }
+//     if (day == 2)
+//     {
+//         Console.Write("Tuesday");
+//     }
+// }
+
